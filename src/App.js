4704bc9f2df2b7f371f80a6cf16239e62012c27b
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-28 17:32:38 +0000

    fonts wip

--- src/App.js
+++ src/App.js
@@ -2,9 +2,30 @@ import React from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import IndexPage from "./pages/index"
 import MapPage from "./pages/map"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+
+  @font-face {
+    font-family: Helvetica Neue;
+    font-weight: 700;
+    src: url(helveticaneue-bold.woff);
+  }
+  @font-face {
+    font-family: Helvetica Neue;
+    font-weight: 400;
+    src: url(helveticaneue-medium.woff);
+  }
+
+  *{
+    font-family: "Helvetica Neue", "Arial" sans-serif;
+  }
+`
+
 
 const App = () =>
     <Router>
+      <GlobalStyle/>
       <Route path="/" component={IndexPage} exact/>
       <Route path="/map" component={MapPage} exact/>
     </Router>
