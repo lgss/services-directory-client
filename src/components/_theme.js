95af0cd23493e492d8136bdaf016c02dd6251392
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-29 19:40:09 +0000

    added colour scheme

---
+++ src/components/_theme.js
@@ -0,0 +1,9 @@
+export default {
+    blue: "#233B8E",
+    darkGrey: "#212121",
+    midGrey: "#999999",
+    lightGrey: "#F7F6F2",
+    brown: "#C7BAA6",
+    green: "#94AC43",
+    maxWidth: "1280px"
+}
