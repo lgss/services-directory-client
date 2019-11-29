8254ce30572e5d227ed4070ee94a6d645fb5de15
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-11-29 22:42:37 +0000

    basic layout done

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -20,26 +20,69 @@ const Container = styled.div`
     margin-right: auto;
 `
 
+const HeaderContainer = styled(Container)`
+    @media screen and (min-width: 600px){
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+    }
+`
+
 const Masthead = styled.a`
     text-decoration: none;
+    &:focus{
+        outline: 3px solid ${theme.focus};
+    }
 `
 
 const SearchForm = styled.form`
     display: none;
+    @media screen and (min-width: 600px){
+        display: flex;
+        flex-direction: row;
+        min-width: 400px;
+    }
 `
 
-const HiddenLabel = styled.label``
+const HiddenLabel = styled.label`
+    font-size: 0;
+    opacity: 0;
+`
 
-const Input = styled.input``
+const Input = styled.input`
+    font-size: 1em;
+    padding: 10px;
+    border: none;
+    flex: 1;
+    &::placeholder{
+        color: ${theme.placeholder};
+    }
+    &:focus{
+        position: relative;
+        outline: 3px solid ${theme.focus};
+        z-index: 2;
+    }
+`
 
 const SearchIcon = styled.img``
 
-const Button = styled.button``
+const Button = styled.button`
+    background: ${theme.grey2};
+    border: none;
+    padding: 10px 25px;
+    &:focus{
+        outline: 3px solid ${theme.focus};
+    }
+`
 
 const Footer = styled.footer`
-    background: ${theme.darkGrey};
+    background: ${theme.grey1};
     color: white;
     padding: 30px 10px;
+    @media screen and (min-width: 600px){
+        padding: 40px 10px;
+    }
 `
 
 const FooterNav = styled.nav`
@@ -48,11 +91,16 @@ const FooterNav = styled.nav`
         display: flex;
         flex-direction: row;
         justify-content: space-between;
+        align-items: center;
+        margin-bottom: 50px;
     }
 `
 
 const FooterLinks = styled.div`
     margin-bottom: 15px;
+    @media screen and (min-width: 600px){
+        margin-bottom: 0px;
+    }
 `
 
 const FooterLink = styled.a`
@@ -63,9 +111,12 @@ const FooterLink = styled.a`
         text-decoration: underline;
     }
     &:focus{
-        color: ${theme.darkGrey};
+        color: ${theme.grey1};
         background: ${theme.focus};
         outline: 3px solid ${theme.focus};
+    }
+    @media screen and (min-width: 600px){
+        margin-right: 15px;
     }
 `
 
@@ -73,6 +124,12 @@ const SocialLinks = styled.div``
 
 const SocialLink = styled.a`
     margin-right: 10px;
+    &:focus{
+        outline: 3px solid ${theme.focus};
+    }
+    @media screen and (min-width: 600px){
+        margin-right: 15px;
+    }
 `
 
 const SocialIcon = styled.img`
@@ -87,7 +144,7 @@ const Layout = ({
 }) =>
     <>
         <Header>
-            <Container fullWidth={fullWidth}>
+            <HeaderContainer fullWidth={fullWidth}>
                 <Masthead href="https://www.buckscc.gov.uk">
                     <img src={masthead} alt="Buckinghamshire Council"/>
                 </Masthead>
@@ -96,10 +153,10 @@ const Layout = ({
                     action="https://www.buckscc.gov.uk/search"
                 >
                     <HiddenLabel htmlFor="q">Search query</HiddenLabel>
-                    <Input name="q" required/>
+                    <Input name="q" required placeholder="Search our websites"/>
                     <Button type="submit"><SearchIcon src={search} alt="Search"/></Button>
                 </SearchForm>
-            </Container>
+            </HeaderContainer>
         </Header>
         {children}
         <Footer>
--- src/components/_theme.js
+++ src/components/_theme.js
@@ -1,8 +1,10 @@
 export default {
     blue: "#233B8E",
-    darkGrey: "#212121",
-    midGrey: "#999999",
-    lightGrey: "#F7F6F2",
+    grey1: "#212121",
+    grey2: "#343434",
+    grey3: "#999999",
+    grey4: "#b6b6b6",
+    grey5: "#F7F6F2",
     brown: "#C7BAA6",
     green: "#94AC43",
     focus: "#FFB81C",
