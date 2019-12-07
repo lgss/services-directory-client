49a79c22b442b4d688a5876a9411128937cbd518
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-07 20:36:04 +0000

    pagination buttons added

--- src/pages/map.js
+++ src/pages/map.js
@@ -22,7 +22,7 @@ const ResultsArea = styled.section`
 `
 
 const ListArea = styled.div`
-    padding: 15px;
+    padding: 15px 15px 50px 15px;
     @media screen and (min-width: 700px){
         width: 500px;
         min-height: 0;
@@ -43,6 +43,57 @@ const CardList = styled.ul`
     list-style: none;
     margin-bottom: 50px;
 `
+
+const NextButton = styled.button`
+    background: none;
+    color: ${theme.blue};
+    border: 2px solid ${theme.blue};
+    padding: 20px 65px;
+    font-size: 1.1rem;
+    font-weight: bold;
+    cursor: pointer;
+    display: block;
+    width: 100%;
+    margin-bottom: 15px;
+    &:hover{
+        filter: brightness(1.3)
+    }
+    &:focus{
+        outline: 3px solid ${theme.focus};
+    }
+    @media screen and (min-width: 700px){
+        display: block;
+        width: auto;
+        margin: 20px auto 10px auto;
+    }
+`
+
+const PrevButton = styled.button`
+    background: none;
+    border: none;
+    color: ${theme.grey2};
+    text-decoration: underline;
+    margin: 10px auto;
+    text-align: center;
+    font-size: 1em;
+    cursor: pointer;
+    display: block;
+    &:hover{
+        text-decoration: none;
+    }
+    &:focus{
+        color: ${theme.grey1};
+        background: ${theme.focus};
+        outline: 3px solid ${theme.focus};
+    }
+`
+
+const P = styled.p`
+    color: ${theme.grey2};
+    margin: 30px 0px 20px 0px;
+    text-align: center;
+`
+
 
 const MapPage = ({
     location
@@ -94,15 +145,23 @@ const MapPage = ({
                             />
                         )}
                     </CardList>
-                    <button onClick={()=>{
+
+                    <P>Showing 20 nearest results</P>
+
+                    <NextButton onClick={()=>{
                         query.page = page + 1
                         history.push(`/map?${queryString.stringify(query)}`)
                         setPage(page + 1)
-                    }}>Next page</button>
+                    }}>Next page</NextButton>
+                    {page > 1 &&
+                        <PrevButton onClick={()=>{
+                            query.page = page - 1
+                            history.push(`/map?${queryString.stringify(query)}`)
+                            setPage(page - 1)
+                        }}>Previous page</PrevButton>
+                    }
                 </ListArea>
                 <MapArea>
-                    {query.lat}
-                    {query.lng}
                     <Map
                         query={query}
                         services={services}
