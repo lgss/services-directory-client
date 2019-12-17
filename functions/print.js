31f4dc7be9a8af309fc8e145eb536cd3a7f743eb
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-17 23:37:47 +0000

    shrug

--- functions/print.js
+++ functions/print.js
@@ -1,25 +1,26 @@
 const generateHtml = require("./generateHtml")
 const pdf = require("html-pdf")
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
     try {
         let shortlist = JSON.parse(event.body)
 
-        pdf.create(generateHtml(shortlist), {
+        await pdf.create(generateHtml(shortlist), {
             format: "Letter",
             orientation: "portrait",
             border: "1in"
-        }).toBuffer((err, buffer) => {
-            console.log("callback triggered")
-            callback(null, {
+        }).toBuffer(async (err, buffer) => {
+            await {
                 statusCode: 200,
                 headers: {
                     "Cache-Control": "no-cache",
                     "Content-Type": "application/pdf",
                 },
-                body: buffer.toString('base64')
-            })
+                body: await Buffer.concat(buffer)
+            }
         })
+
+
     } catch(err) {
         return {
             statusCode: 500,
