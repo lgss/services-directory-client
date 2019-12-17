fd34af2575f8dfa7c0141c4cef9a800b42b95be4
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2019-12-17 15:39:15 +0000

    change netlify.toml so it doesn't break the cli, and add a empty function

---
+++ functions/print.js
@@ -0,0 +1,5 @@
+exports.handler = function(event, context, callback) {
+    // 1. Suck up the query string
+    // 2. Request data from the services API
+    // 3. Render and respond with a PDF for printing
+}
