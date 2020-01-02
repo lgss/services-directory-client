572f148043ab8357d7f69d1abf9ccf8450de268c
Jaye Hackett <jayehackett@FutureGovs-MacBook-Pro.local>
2020-01-02 11:43:37 +0000

    guuurp

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "react-app-polyfill/ie11"
 import "react-app-polyfill/stable"
 import React from "react"
 import App from "./App"
-import { render } from "react-dom";
+import { render } from "react-dom"
 
 const rootElement = document.getElementById("root")
 
